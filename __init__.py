3bb30ac063f9469af78efbf49dcb1decc935c12c
masak <mayamisaki@gmail.com>
2024-01-23 13:52:05 +0900

    a古いコード削除

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Masak GeneralAddon",
     "author": "Masashi Kamiyama( masak )",
     "version": (1,0),
-    "blender": (2, 80, 0),
+    "blender": (4, 00, 0),
     "location": "View3D > Tool Shelf > Masak VariousHelper",
     "description": "various helper class and method for various projects",
     "warning": "",
@@ -42,17 +42,14 @@ import bpy
 
 # クラスをまとめる
 classes = (
-    #obj_generate_ctr_bones.MskGenerateCtrBones,
     obj_generate_ctr_bones_selected.MskGenerateCtrBonesSelected,
-    #obj_generate_leg_rig.MskGenerateLegRig,
     obj_generate_shapekey_bones.MskGenerateShapeKeyBones,
-    #obj_rename_bones_vroid_to_bl.MskRenameBonesVroidToBL,
-    #obj_rename_groups_vroid_to_bl.MskRenameGroupsVroidToBL,
 
 )
 
 # まとめたクラスを一度に登録
 register, unregister = bpy.utils.register_classes_factory(classes)
 
-#if __name__ == "__main__":
-    #register()
+
+if __name__ == "__main__":
+    register()
